e008c9f79991ddb8f1eaf68aa6a937577e8d0ef7
Md. Zahid Hasan Jamil <mzhj19@gmail.com>
2023-09-22 14:04:10 +0600

    Initial commit

---
+++ MasteringSpringSecurity/src/main/java/com/ZahidHasanJamil/MasteringSpringSecurity/MasteringSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.ZahidHasanJamil.MasteringSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteringSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteringSpringSecurityApplication.class, args);
+	}
+
+}
